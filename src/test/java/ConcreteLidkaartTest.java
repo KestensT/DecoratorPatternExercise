8723ae77ba957ca192cd6c3f8d71f965f941d19f
Thibault Kestens <thibault.kestens@cegeka.com>
2017-10-24 14:18:23 +0200

    solved

--- src/test/java/ConcreteLidkaartTest.java
+++ src/test/java/ConcreteLidkaartTest.java
@@ -1,3 +1,4 @@
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,11 +9,37 @@ public class ConcreteLidkaartTest {
         Lidkaart lidkaart = new ConcreteLidkaart();
         int expected = 100;
 
+        Assertions.assertThat(lidkaart.getPrice()).isEqualTo(expected);
+
 
     }
 
     @Test
     public void addingKlimmuurToLidkaart_shouldAddToTotal() throws Exception {
+        Lidkaart lidkaart = new ConcreteLidkaart();
+
+        int expected = 115;
+
+        lidkaart = new Klimmuur(lidkaart);
+
+        int actual = lidkaart.getPrice();
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void addingEverythingToLidkaart_shouldAddToTotal() throws Exception {
+        Lidkaart lidkaart = new ConcreteLidkaart();
+
+        int expected = 185;
+
+        lidkaart = new Klimmuur(lidkaart);
+        lidkaart = new Zwembad(lidkaart);
+        lidkaart = new Groepslessen(lidkaart);
+
+        int actual = lidkaart.getPrice();
+
+        Assertions.assertThat(actual).isEqualTo(expected);
 
     }
 }
